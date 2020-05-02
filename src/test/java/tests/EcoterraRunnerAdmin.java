c38e3c05e552c83f00d5d9f2e40fd141984f321b
adapaleela <48840620+adapaleela@users.noreply.github.com>
2020-05-02 14:45:03 +0530

    Update EcoterraRunnerAdmin.java

--- src/test/java/tests/EcoterraRunnerAdmin.java
+++ src/test/java/tests/EcoterraRunnerAdmin.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features= {"E:\\leelajava\\ecoterra.com\\src\\test\\resources\\feature\\ecoterra_admin.feature"},
                  monochrome=true,                             
-                 glue={"classpath:glue"},
+                
                  plugin={"pretty","html:target\\result2","rerun:target\\failed.txt"}
 				)
 public class EcoterraRunnerAdmin
